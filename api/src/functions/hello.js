5ff007606b41d5aeb32b0b118744da5c8e4e2904
gitfemto <lupapakekolor@gmail.com>
2026-06-27 05:16:32 -0400

    Simplify function to test deployment without Cosmos DB

--- api/src/functions/hello.js
+++ api/src/functions/hello.js
@@ -1,19 +1,14 @@
 const { app } = require('@azure/functions');
-const { CosmosClient } = require('@azure/cosmos');
-
-const client = new CosmosClient(process.env.COSMOS_CONNECTION_STRING);
-const container = client.database('hellodb').container('messages');
 
 app.http('hello', {
     methods: ['GET'],
     authLevel: 'anonymous',
     route: 'hello',
     handler: async (request, context) => {
-        const { resource } = await container.item('1', '1').read();
         return {
             status: 200,
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ message: resource?.message ?? 'Hello from Azure!' })
+            body: JSON.stringify({ message: 'Hello from Azure Functions!' })
         };
     }
 });
